8fc00a8db0f20a1e3bb4d6e667cfb7345e1eb53d
siccoo <obichilaka@gmail.com>
2020-02-06 10:12:28 +0100

    Initial commit for sales server

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const { addUser, removeUser, getUser, getUserInRoom } = require('./user');
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    socket.on('join', ({ name, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room });
+
+        if(error) {
+            return callback(error);
+        };
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, Welcome to the Room ${user.room}` });
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!` });
+        
+        socket.join(user.room);
+    });
+    
+    socket.on('disconnect', () => {
+        console.log("User had left!!!");
+        
+    })
+})
+
+app.use(router);    
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`))
